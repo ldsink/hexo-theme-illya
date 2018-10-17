cdcd7b09ecfa8cbdc1d541ebe9b78093cffdd2fc
ZHOU Cheng <c.zhou@live.com>
2018-10-17 17:58:32 +0800

    use webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,88 @@
-var webpack = require("webpack");
-var autoprefixer = require('autoprefixer');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanPlugin = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// 模板压缩
-// 详见：https://github.com/kangax/html-minifier#options-quick-reference
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
-var minifyHTML = {
-  collapseInlineTagWhitespace: true,
-  collapseWhitespace: true,
-  minifyJS:true
-}
+const minifyHTML = {
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    minifyJS: true
+};
 
 module.exports = {
-  entry: {
-    main: "./source-src/js/main.js",
-    slider: "./source-src/js/slider.js",
-    mobile: ["babel-polyfill", "./source-src/js/mobile.js"]
-  },
-  output: {
-    path: "./source",
-    publicPath: "./",
-    filename: "[name].[chunkhash:6].js"
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader?cacheDirectory',
-      exclude: /node_modules/
-    }, {
-      test: /\.html$/,
-      loader: 'html'
-    }, {
-      test: /\.(scss|sass|css)$/,
-      loader: ExtractTextPlugin.extract('style-loader', ['css-loader?-autoprefixer', 'postcss-loader', 'sass-loader'])
-    }, {
-      test: /\.(gif|jpg|png)\??.*$/,
-      loader: 'url-loader?limit=500&name=img/[name].[ext]'
-    }, {
-      test: /\.(woff|svg|eot|ttf)\??.*$/,
-      loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
-    }]
-  },
-  alias: {
-    'vue$': 'vue/dist/vue.js'
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
-  // devtool: '#eval-source-map',
-  postcss: function() {
-    return [autoprefixer];
-  },
-  plugins: [
-    new ExtractTextPlugin('[name].[chunkhash:6].css'),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      minify: minifyHTML,
-      template: './source-src/script.ejs',
-      filename: '../layout/_partial/script.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      minify: minifyHTML,
-      template: './source-src/css.ejs',
-      filename: '../layout/_partial/css.ejs'
-    })
-  ],
-  watch: true
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new CleanPlugin('builds')
-  ])
-}
+    entry: {
+        main: "./source-src/js/main.js",
+        slider: "./source-src/js/slider.js",
+        mobile: ["babel-polyfill", "./source-src/js/mobile.js"]
+    },
+    output: {
+        path: __dirname + "/source",
+        // publicPath: "./",
+        filename: "[name].[chunkhash:6].js"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader?cacheDirectory',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(gif|jpg|png)\??.*$/,
+                loader: 'url-loader?limit=500&name=img/[name].[ext]'
+            },
+            {
+                test: /\.(woff|svg|eot|ttf)\??.*$/,
+                loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+        ]
+    },
+    // resolve: {
+    //     alias: {
+    //         'vue$': 'vue/dist/vue.common.js'
+    //     }
+    // },
+    // plugins: [
+    //     new ExtractTextPlugin('[name].[chunkhash:6].css'),
+    //     new webpack.DefinePlugin({
+    //         'process.env.NODE_ENV': '"production"'
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         inject: false,
+    //         cache: false,
+    //         minify: minifyHTML,
+    //         template: './source-src/script.ejs',
+    //         filename: '../layout/_partial/script.ejs'
+    //     }),
+    //     new HtmlWebpackPlugin({
+    //         inject: false,
+    //         cache: false,
+    //         minify: minifyHTML,
+    //         template: './source-src/css.ejs',
+    //         filename: '../layout/_partial/css.ejs'
+    //     })
+    // ],
+    // watch: true
+};
