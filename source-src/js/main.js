82ab68bd7981e79aa40651fccc3961961456d5a7
ZHOU Cheng <c.zhou@live.com>
2018-10-19 12:00:49 +0800

    style:code foramt

--- source-src/js/main.js
+++ source-src/js/main.js
@@ -1,18 +1,18 @@
 // 样式
-import '../css/main.scss'
+import '../css/main.scss';
 // 上报
-import './report'
+import './report';
 // 图片查看器
-import Viewer from './viewer'
+import Viewer from './viewer';
 // 分享
-import Share from './share'
+import Share from './share';
 // 边缘
-import Aside from './aside'
+import Aside from './aside';
 
-import {addLoadEvent} from './util'
+import {addLoadEvent} from './util';
 
-addLoadEvent(function() {
-	Share.init()
-	Viewer.init()
-	Aside.init()
-})
+addLoadEvent(function () {
+  Share.init();
+  Viewer.init();
+  Aside.init();
+});
