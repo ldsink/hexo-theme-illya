987d2881186f2bd8f721908b446161272742c4eb
ZHOU Cheng <c.zhou@live.com>
2018-10-19 11:36:02 +0800

    release: 2594c257f32b1bff7aa1

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: devMode ? '[name].css' : '[name].[hash:6].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash:6].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash:6].css'
         }),
         new HtmlWebpackPlugin({
             inject: false,
