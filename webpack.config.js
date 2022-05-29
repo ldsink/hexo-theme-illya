85ca1caf240362ab214b0241e882540ff6070cae
ZHOU Cheng <c.zhou@live.com>
2022-05-29 18:23:53 +0800

    使用 Webpack 5.0；更新全局字体样式；修复翻页符号渲染失败问题。 (#19)
    * update packages
    
    * update date
    
    * update node version
    
    * update package version
    
    * style: code format
    
    * fix quote bug
    
    * style: use MiSans
    
    * style: change font family and font weight

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,110 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: {
-    main: "./source-src/js/main.js",
-    slider: "./source-src/js/slider.js",
-    mobile: ["babel-polyfill", "./source-src/js/mobile.js"]
-  },
-  output: {
-    path: __dirname + "/source",
-    publicPath: "./",
-    filename: "[name].[chunkhash:6].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader?cacheDirectory',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(gif|jpg|png)\??.*$/,
-        loader: 'url-loader?limit=500&name=img/[name].[ext]'
-      },
-      {
-        test: /\.(woff|svg|eot|ttf)\??.*$/,
-        loader: "file-loader?name=fonts/[name].[hash:6].[ext]"
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['source/*']),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: devMode ? '[name].css' : '[name].[hash:6].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash:6].css'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      template: './source-src/css.ejs',
-      filename: '../layout/_partial/css.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      cache: false,
-      template: './source-src/script.ejs',
-      filename: '../layout/_partial/script.ejs'
-    })
-  ],
-  watch: devMode
+module.exports = (env, argv) => {
+  const devMode = argv.mode === "development";
+  return {
+    watch: devMode,
+    entry: {
+      main: "./source-src/js/main.js",
+      slider: "./source-src/js/slider.js",
+      mobile: ["babel-polyfill", "./source-src/js/mobile.js"],
+    },
+    output: {
+      path: __dirname + "/source",
+      publicPath: "./",
+      filename: "[name].[chunkhash:7].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 2,
+                // 0 => no loaders (default);
+                // 1 => postcss-loader;
+                // 2 => postcss-loader, sass-loader
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-import",
+                      "postcss-preset-env",
+                      "cssnano",
+                    ],
+                  ],
+                },
+              },
+            },
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif|webp)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name].[hash:7][ext][query]'
+          },
+        },
+        {
+          test: /\.(woff|eot|ttf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name].[hash:7][ext][query]'
+          },
+        },
+        {
+          test: /\.html$/i,
+          loader: "html",
+        },
+      ],
+    },
+    plugins: [
+      // new CleanWebpackPlugin(["source/*"]),
+      new CleanWebpackPlugin({ verbose: true }),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: devMode ? "[name].css" : "[name].[chunkhash:7].css",
+        chunkFilename: devMode ? "[id].css" : "[id].[chunkhash:7].css",
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        cache: false,
+        template: "./source-src/css.ejs",
+        filename: "../layout/_partial/css.ejs",
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        cache: false,
+        template: "./source-src/script.ejs",
+        filename: "../layout/_partial/script.ejs",
+      }),
+    ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin({ extractComments: false }),
+      ],
+    },
+  };
 };
