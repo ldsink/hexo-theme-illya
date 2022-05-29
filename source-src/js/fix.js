197ba0c761a160764c6f50c4eaacfeaa14b4014b
ZHOU Cheng <c.zhou@live.com>
2022-05-29 17:34:02 +0800

    fix quote bug

--- source-src/js/fix.js
+++ source-src/js/fix.js
@@ -2,10 +2,10 @@ function init() {
   // 由于hexo分页不支持，手工美化
   const $nav = document.querySelector('#page-nav');
   if ($nav && !document.querySelector('#page-nav .extend.prev')) {
-    $nav.innerHTML = '<a class="extend prev disabled" rel="prev">&laquo; Prev</a>' + $nav.innerHTML;
+    $nav.innerHTML = '<a class="extend prev disabled" rel="prev">« Prev</a>' + $nav.innerHTML;
   }
   if ($nav && !document.querySelector('#page-nav .extend.next')) {
-    $nav.innerHTML = $nav.innerHTML + '<a class="extend next disabled" rel="next">Next &raquo;</a>';
+    $nav.innerHTML = $nav.innerHTML + '<a class="extend next disabled" rel="next">Next »</a>';
   }
 
   // 新窗口打开
